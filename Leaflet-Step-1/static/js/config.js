1e641b1ca1a52e1136054d000f605cec5554262a
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-03-17 16:51:41 -0400

    update

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,6 @@
-// API key
-const API_KEY = "YOUR API KEY HERE!";
+
+
+// // Import private info for API key
+// code_source_passcode = open('/Users/Richa/Desktop/API_private_connect.js');
+// // API key
+// const API_KEY = code_source_passcode.read();
